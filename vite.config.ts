dd10c53c1a36256680de16c8dd869703fae59783
Declan Allen <deckallen@hotmail.co.uk>
2026-04-07 16:51:03 +0100

    no base needed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hiphopopotamuss.github.io/',
   server: {
     allowedHosts: ['personal-site.test', 'https://hiphopopotamuss.github.io/#projects'],
   },
