e1b50804109edfae236f2482e70c031324503e61
Declan Allen <deckallen@hotmail.co.uk>
2026-04-07 17:11:10 +0100

    hello golland

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Welcome to my vite app</h1>
+          <h1>Hello Mister Golland</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
